eda69b27e8f0f178ec1a007dc8afa47b2774c908
ewal2 <38157680+ewal2@users.noreply.github.com>
2019-01-02 22:12:08 +0100

    Update

--- calc-second-version.js
+++ calc-second-version.js
@@ -1,4 +1,3 @@
-
 var a = document.querySelector("#a").value;
 var b = document.querySelector("#b").value;
 
@@ -16,3 +15,49 @@ function suma() {
   }
 }
 
+function roznica() {
+  var a = document.querySelector("#a").value;
+  var b = document.querySelector("#b").value;
+  if(a === "" || b === ""){
+    document.querySelector("#wynik").innerHTML = "Prosze uzupelnic obie liczby.";
+  } else{
+    a = parseFloat(a);
+    b = parseFloat(b);
+    
+    var roznica = a - b;
+    document.querySelector("#wynik").innerHTML = "Wynik dzialania wynosi: " + roznica;
+  }
+}
+
+function iloczyn() {
+  var a = document.querySelector("#a").value;
+  var b = document.querySelector("#b").value;
+  if(a === "" || b === ""){
+    document.querySelector("#wynik").innerHTML = "Prosze uzupelnic obie liczby.";
+  } else{
+    a = parseFloat(a);
+    b = parseFloat(b);
+    
+    var iloczyn = a * b;
+    document.querySelector("#wynik").innerHTML = "Wynik dzialania wynosi: " + iloczyn;
+  }
+}
+
+function iloraz() {
+  var a = document.querySelector("#a").value;
+  var b = document.querySelector("#b").value;
+  if(a === "" || b === ""){
+    document.querySelector("#wynik").innerHTML = "Prosze uzupelnic obie liczby.";
+  } else{
+    a = parseFloat(a);
+    b = parseFloat(b);
+    
+    if(b === 0){
+      document.querySelector("#wynik").innerHTML = "Nie wolno dzielic przez 0.";
+    }
+    else{
+      var iloraz = a / b;
+      document.querySelector("#wynik").innerHTML = "Wynik dzialania wynosi: " + iloraz;
+    }
+  }
+}
