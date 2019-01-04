9721ed48cdfe1ff13bf836390a56398136d69a0b
ewal2 <38157680+ewal2@users.noreply.github.com>
2019-01-04 17:50:51 +0100

    Refactor calc-fourth.js

--- calc-fourth/calc-fourth.js
+++ calc-fourth/calc-fourth.js
@@ -1,66 +1,59 @@
-let plusButton = document.querySelector(".plus");
-let minusButton = document.querySelector(".minus");
-let multiplyButton = document.querySelector(".multi");
-let divideButton = document.querySelector(".divide");
-let resetButton = document.querySelector(".reset");
-let resultDisplay = document.querySelector("p span");
-
-plusButton.addEventListener("click", function(){
+document.querySelector(".plus").addEventListener("click", function(){
 let a = document.querySelector("#a").value;
 let b = document.querySelector("#b").value; 
 if(a === "" || b === ""){
-  resultDisplay.textContent = "Prosze uzupelnic obie liczby."
+  document.querySelector("p span").textContent = "Prosze uzupelnic obie liczby."
 }  else {
   a = parseFloat(a);
   b = parseFloat(b);
-  resultDisplay.textContent = a + b;  
+  document.querySelector("p span").textContent = a + b;  
   }
 });
 
-minusButton.addEventListener("click", function(){
+document.querySelector(".minus").addEventListener("click", function(){
 let a = document.querySelector("#a").value;
 let b = document.querySelector("#b").value; 
 if(a === "" || b === ""){
-  resultDisplay.textContent = "Prosze uzupelnic obie liczby."
+  document.querySelector("p span").textContent = "Prosze uzupelnic obie liczby."
 } else{
   a = parseFloat(a);
   b = parseFloat(b);
-  resultDisplay.textContent = a - b;
+  document.querySelector("p span").textContent = a - b;
   }
 });
 
 
-multiplyButton.addEventListener("click", function(){
+document.querySelector(".multi").addEventListener("click", function(){
 let a = document.querySelector("#a").value;
 let b = document.querySelector("#b").value;
 if(a === "" || b === ""){
-  resultDisplay.textContent = "Prosze uzupelnic obie liczby."
+  document.querySelector("p span").textContent = "Prosze uzupelnic obie liczby."
 } else{
   a = parseFloat(a);
   b = parseFloat(b);
-  resultDisplay.textContent = a * b;
+  document.querySelector("p span").textContent = a * b;
   }
 });
 
 
-divideButton.addEventListener("click", function(){
+document.querySelector(".divide").addEventListener("click", function(){
 let a = document.querySelector("#a").value;
 let b = document.querySelector("#b").value;
 if(a === "" || b === ""){
-  resultDisplay.textContent = "Prosze uzupelnic obie liczby."
+  document.querySelector("p span").textContent = "Prosze uzupelnic obie liczby."
 } else if(b == 0){
   a = parseInt(a);
   b = parseInt(b);
-  resultDisplay.textContent = "Nie wolno dzielic przez 0."  
+  document.querySelector("p span").textContent = "Nie wolno dzielic przez 0."  
 }
 else{
     a = parseFloat(a);
     b = parseFloat(b);
-    resultDisplay.textContent = a / b;
+    document.querySelector("p span").textContent = a / b;
 }
 });
 
-resetButton.addEventListener("click", function(){
+document.querySelector(".reset").addEventListener("click", function(){
 reset();
 });
 
